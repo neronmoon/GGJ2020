c290e215b9e196e1c8c18e432eef5607ea5b00a4
neronmoon <alistar.neron@gmail.com>
2020-02-01 16:36:33 +0200

    Some stuff

--- Assets/Source/GGJ2020/Features/MovementFeature/PositionCalculator.cs
+++ Assets/Source/GGJ2020/Features/MovementFeature/PositionCalculator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Source.GGJ2020.Features.MovementFeature {
     public class PositionCalculator {
         public static Vector3 Calculate(Vector2Int pos) {
-            return new Vector3(pos.x + 0.5f, pos.y, 0f);
+            return new Vector3(pos.x + 0.5f, pos.y + 0.5f, 0f);
         }
     }
 }
