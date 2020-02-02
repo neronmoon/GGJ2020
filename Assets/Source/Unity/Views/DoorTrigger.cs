5162bb82e77988f8098bde1ebeef553858f48854
neronmoon <alistar.neron@gmail.com>
2020-02-02 11:30:15 +0200

    Non overlapping skeletons

--- Assets/Source/Unity/Views/DoorTrigger.cs
+++ Assets/Source/Unity/Views/DoorTrigger.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 namespace Source.Unity.Views {
     public class DoorTrigger : MonoBehaviour {
         public DoorPosition[] Doors;
+        public bool ShouldStandToOpen = true;
 
         private void OnTriggerEnter2D(Collider2D collision) {
             foreach (var door in Doors) {
-                door.Open();                
+                door.Open();
             }
-            
         }
+
         private void OnTriggerExit2D(Collider2D collision) {
+            if (!ShouldStandToOpen) {
+                return;
+            }
             foreach (var door in Doors) {
                 door.Close();
             }
